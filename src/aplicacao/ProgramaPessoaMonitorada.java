31653629da8246777fa4c4a6bc91359f05d4bdab
laislealdina <laisdina@id.uff.br>
2018-07-20 23:05:47 -0300

    Aula 1 e Aula 2 de JPA - Inserindo, recuperando e removendo pessoa do banco usando JPA.(API JPA mapeamento objeto-relacional)

---
+++ src/aplicacao/ProgramaPessoaMonitorada.java
@@ -0,0 +1,28 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class ProgramaPessoaMonitorada {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		
+		
+		em.remove(p);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		
+		System.out.println("Pronto!");
+
+	}
+
+}
